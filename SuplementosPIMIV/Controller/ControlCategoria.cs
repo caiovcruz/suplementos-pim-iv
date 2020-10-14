7883c58002a06a96f932189f873cc25a6d7eefc5
Caio Cruz <caio.vcruz@outlook.com>
2020-10-14 15:27:13 -0300

    Criado a classe ControlCategoria que herda a ModelCategoria

---
+++ SuplementosPIMIV/Controller/ControlCategoria.cs
@@ -0,0 +1,22 @@
+﻿using PontoDeVenda.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PontoDeVenda.Control
+{
+    public class ControlCategoria : ModelCategoria
+    {
+        public ControlCategoria() { }
+
+        public ControlCategoria(string nm_categoria, string ds_categoria) : base(nm_categoria, ds_categoria) { }
+
+        public ControlCategoria(int id_categoria, string nm_categoria, string ds_categoria) : 
+            base(id_categoria, nm_categoria, ds_categoria) { }
+
+        public ControlCategoria(string nm_categoria) : base(nm_categoria) { }
+
+        public ControlCategoria(int id_categoria) : base(id_categoria) { }
+    }
+}

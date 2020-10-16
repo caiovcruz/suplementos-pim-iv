a78d6c010edf762805c9c0fa8c424d8d0a10a9a3
Caio Cruz <caio.vcruz@outlook.com>
2020-10-16 16:11:51 -0300

    Criada as classes ModelMarca com suas propriedades e metodos necessarios, e ControllerMarca que herda a ModelMarca

---
+++ SuplementosPIMIV/Controller/ControllerMarca.cs
@@ -0,0 +1,19 @@
+﻿using SuplementosPIMIV.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuplementosPIMIV.Controller
+{
+    public class ControllerMarca : ModelMarca
+    {
+        public ControllerMarca() : base() { }
+
+        public ControllerMarca(string nm_marca, bool incluir) : base(nm_marca, incluir) { }
+
+        public ControllerMarca(int id_marca, string nm_marca) : base(id_marca, nm_marca) { }
+
+        public ControllerMarca(int id_marca) : base(id_marca) { }
+    }
+}
---
+++ SuplementosPIMIV/Model/ModelMarca.cs
@@ -0,0 +1,173 @@
+﻿using DataBase;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace SuplementosPIMIV.Model
+{
+    public class ModelMarca
+    {
+        private DAO dataAcessObject;
+
+        public int ID_Marca { get; set; }
+        public string NM_Marca { get; set; }
+        public string DS_Mensagem { get; set; }
+
+        public ModelMarca() { }
+
+        public ModelMarca(string nm_marca, bool incluir)
+        {
+            NM_Marca = nm_marca;
+
+            if (incluir)
+            {
+                Incluir();
+            }
+        }
+
+        public ModelMarca(int id_marca, string nm_marca)
+        {
+            ID_Marca = id_marca;
+            NM_Marca = NM_Marca;
+
+            Alterar();
+        }
+
+        public ModelMarca(int id_marca)
+        {
+            ID_Marca = id_marca;
+
+            Excluir();
+        }
+
+        private void SetConnection()
+        {
+            dataAcessObject = new DAO();
+            dataAcessObject.Setup(DataBase.DatabaseTypes.MySql,
+                "Persist Security Info=False; " +
+                "Server=localhost; " +
+                "Database=pdv_suplementos; " +
+                "Uid=root; " +
+                "Pwd=011118");
+        }
+
+        public void Incluir()
+        {
+            DS_Mensagem = "";
+
+            SetConnection();
+            if (dataAcessObject.Connector.Open())
+            {
+                string SQLInsert = "INSERT INTO tb_marca " +
+                    "(" +
+                        "NM_Marca, " +
+                        "Ativo" +
+                    ")" +
+                    "VALUES " +
+                    "(" +
+                        "'" + NM_Marca + "', " +
+                        "1" +
+                    ")";
+                var result = dataAcessObject.Connector.Execute(SQLInsert);
+
+                DS_Mensagem = result > 0 ? "OK" : "Erro ao cadastrar";
+            }
+            else
+            {
+                DS_Mensagem = "Erro de conexão";
+            }
+        }
+
+        public void Alterar()
+        {
+            DS_Mensagem = "";
+
+            SetConnection();
+            if (dataAcessObject.Connector.Open())
+            {
+                string SQLUpdate = "UPDATE tb_sabor SET " +
+                    "NM_Marca = '" + NM_Marca + "' " +
+                    "WHERE ID_Marca = '" + ID_Marca + "'";
+                var result = dataAcessObject.Connector.Execute(SQLUpdate);
+
+                DS_Mensagem = result > 0 ? "OK" : "Erro ao alterar";
+            }
+            else
+            {
+                DS_Mensagem = "Erro de conexão";
+            }
+        }
+
+        public DataTable Consultar()
+        {
+            DataTable dataTable = new DataTable();
+
+            SetConnection();
+            if (dataAcessObject.Connector.Open())
+            {
+                string SQLSelect = "SELECT " +
+                    "ID_Marca, " +
+                    "NM_Marca " +
+                    "FROM tb_marca " +
+                    "WHERE Ativo = 1 " +
+                    "AND NM_Marca LIKE CONCAT('" + NM_Marca + "', '%') " +
+                    "ORDER BY ID_Marca DESC";
+                IDataReader dataReader = dataAcessObject.Connector.QueryWithReader(SQLSelect);
+                dataTable.TableName = "tb_marca";
+                dataTable.Load(dataReader);
+            }
+            else
+            {
+                dataTable = null;
+            }
+
+            return dataTable;
+        }
+
+        public void Excluir()
+        {
+            DS_Mensagem = "";
+
+            SetConnection();
+            if (dataAcessObject.Connector.Open())
+            {
+                string SQLDelete = "UPDATE tb_marca SET Ativo = 0 WHERE ID_Marca = '" + ID_Marca + "'";
+                var result = dataAcessObject.Connector.Execute(SQLDelete);
+
+                DS_Mensagem = result > 0 ? "OK" : "Erro ao excluir";
+            }
+            else
+            {
+                DS_Mensagem = "Erro de conexão";
+            }
+        }
+
+        public DataTable Exibir()
+        {
+            DataTable dataTable = new DataTable();
+
+            SetConnection();
+            if (dataAcessObject.Connector.Open())
+            {
+                string SQLSelect = "SELECT " +
+                    "ID_Marca, " +
+                    "NM_Marca " +
+                    "FROM tb_marca " +
+                    "WHERE Ativo = 1 " +
+                    "ORDER BY ID_Marca DESC";
+                IDataReader dataReader = dataAcessObject.Connector.QueryWithReader(SQLSelect);
+                dataTable.TableName = "tb_marca";
+                dataTable.Load(dataReader);
+            }
+            else
+            {
+                dataTable = null;
+                DS_Mensagem = "Erro de conexão";
+            }
+
+            return dataTable;
+        }
+    }
+}
